5660573b1d248409d8b69ccb2ae3001418beda5b
Inoland <contact@nobandegan.com>
2025-08-01 13:29:07 +0400

    update unzip and add remove file

--- src/inopyutils/file_helper/file_helper.py
+++ src/inopyutils/file_helper/file_helper.py
@@ -6,6 +6,17 @@ class InoFileHelper:
     @staticmethod
     async def unzip(zip_path: Path, output_path: Path) -> dict:
         output_path.mkdir(parents=True, exist_ok=True)
+        if not zip_path.is_file():
+            return {
+                "success": False,
+                "msg": f"{zip_path.name} is not a file"
+            }
+
+        if not zip_path.suffix == ".zip":
+            return {
+                "success": False,
+                "msg": f"{zip_path.name} is not a zip file"
+            }
 
         def _extract():
             with zipfile.ZipFile(zip_path, "r") as zf:
@@ -35,3 +46,30 @@ class InoFileHelper:
                 "success": False,
                 "msg": f"Error extracting {zip_path.name}: {e}"
             }
+
+    @staticmethod
+    async def remove_file(file_path: Path) -> dict:
+        if not file_path.exists():
+            return {
+                "success": False,
+                "msg": f"{file_path.name} not exist"
+            }
+
+        if not file_path.is_file():
+            return {
+                "success": False,
+                "msg": f"{file_path.name} is not a file"
+            }
+
+        try:
+            await asyncio.to_thread(file_path.unlink)
+        except Exception as e:
+            return {
+                "success": False,
+                "msg": f"⚠️ Failed to delete {file_path}: {e}"
+            }
+
+        return {
+            "success": True,
+            "msg": f"File {file_path.name} deleted"
+        }
