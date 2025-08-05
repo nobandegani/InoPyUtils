d06d62420373c315a11ab0dd5b75a064f5f18f85
Inoland <contact@nobandegan.com>
2025-08-06 01:16:49 +0400

    add spark helper

--- src/inopyutils/__init__.py
+++ src/inopyutils/__init__.py
@@ -1,10 +1,12 @@
 from .meida_helper.media_helper import InoMediaHelper
 from .config_helper.config_helper import InoConfigHelper
 from .file_helper.file_helper import InoFileHelper
-from .spark_helper.spark_helper import SparkWorkflows
+from .spark_helper.spark_helper import SparkHelper,SparkWorkflows
 
 __all__ = [
     "InoConfigHelper",
     "InoMediaHelper",
     "InoFileHelper",
-    "SparkWorkflows"]
+    "SparkHelper",
+    "SparkWorkflows"
+]
--- src/inopyutils/spark_helper/spark_helper.py
+++ src/inopyutils/spark_helper/spark_helper.py
@@ -1,13 +1,49 @@
-from enum import Enum, auto
-from dataclasses import dataclass
+from enum import Enum
+from inocloudreve import CloudreveClient
+
+from ..file_helper.file_helper import InoFileHelper
+
 
 class SparkWorkflows(Enum):
-    CAPTION_GENERATOR        = "caption_generator"
-    DATASET_IMAGE_GENERATOR  = "dataset_image_generator"
-    FACE_GENERATOR           = "face_generator"
-    FACE_TOOL                = "face_tool"
-    IMAGE_GENERATOR          = "image_generator"
-    VIDEO_GENERATOR          = "video_generator"
-    FACE_SWAPPER             = "face_swapper"
+    CAPTION_GENERATOR        = "CaptionGenerator"
+    DATASET_IMAGE_GENERATOR  = "DatasetImageGenerator"
+    FACE_GENERATOR           = "FaceGenerator"
+    FACE_TOOL                = "FaceTool"
+    IMAGE_GENERATOR          = "ImageGenerator"
+    VIDEO_GENERATOR          = "VideoGenerator"
+    FACE_SWAPPER             = "FaceSwapper"
 
+class SparkHelper:
+    @staticmethod
+    async def get_batch_folder(cloud_client: CloudreveClient, workflow: SparkWorkflows, creator_name: str) -> dict:
+        uri = f"Spark/Creators/{creator_name}/{workflow.FACE_SWAPPER.value}"
 
+        last_folder_res = await cloud_client.get_last_folder_or_file(
+            uri=uri
+        )
+        if not last_folder_res["status_code"] == 200 :
+            return last_folder_res
+
+        empty_folder = last_folder_res["last"] is None
+        if empty_folder:
+            batch_uri = uri + "/Batch_00001"
+        else:
+            last_name = last_folder_res["last_name"]
+            increased_name = InoFileHelper.increment_batch_name(last_name)
+            batch_uri = uri + "/" + increased_name
+        return {
+            "success": True,
+            "msg": "getting batch folder successful",
+            "uri": batch_uri
+        }
+
+    @staticmethod
+    def get_default_storage_policy() -> dict:
+        return {
+            "success": True,
+            "msg": "",
+            "id": "O8cN",
+            "name": "SparkDrive-2",
+            "type": "s3",
+            "max_size": 0
+        }
