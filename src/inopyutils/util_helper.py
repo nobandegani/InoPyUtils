ed3c1e96601cbf515c8ea4cbaa5dae6d45b754b1
Inoland <contact@nobandegan.com>
2025-11-07 21:11:05 +0400

    Update util_helper.py

--- src/inopyutils/util_helper.py
+++ src/inopyutils/util_helper.py
@@ -8,6 +8,16 @@ def ok(msg: str = "success", **extra: Any) -> Dict[str, Any]:
 def err(msg: str = "error", **extra: Any) -> Dict[str, Any]:
     return {"success": False, "msg": msg, **extra}
 
+def is_err(res: Any):
+    if isinstance(res, tuple):
+        if all(i is not None for i in res):
+            res = res[0]
+        else:
+            return True
+    if isinstance(res, dict):
+        return not res.get("success", False)
+
+    return False
 
 class InoUtilHelper:
     @staticmethod
